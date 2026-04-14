8b16337227a084d76fa97fefa9bb1da7a84e1df7
Celicey <celiceyavalon@gmail.com>
2026-04-14 10:37:16 -0500

    Added some information to the account types.

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -7,11 +7,13 @@ public abstract class BankAccount {
   
     private final String accountName;
     private String password;
+    private final String accountType;
     
 
-    public BankAccount(String accountName, String password) {
+    public BankAccount(String accountName, String password, String accountType) {
         this.accountName = accountName;
         setPassword(password);
+        this.accountType = accountType;
     }
 
 
@@ -32,6 +34,19 @@ public abstract class BankAccount {
     public String getAccountName() { //used for printing the account name in the main menu
         return this.accountName;
     }
+
+
+    // Gets the current account type
+    public String getAccountType() {
+        return this.accountType;
+    }
+
+    // Sets the current account type 
+    public String setAccountType(String accountType) {
+        return this.accountType;
+    }
+
+
 
     public abstract void transferMoney(BankAccount toAccount, double transferAmount);
 
